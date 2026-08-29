a87e92925b663fa6d2d0ff747bccf16d0b170716
wdarney <119882677+wdarney@users.noreply.github.com>
2026-08-29 14:25:04 -0400

    Fix Windows R2IQ SIMD helper linkage

--- Core/fft_mt_r2iq/fft_mt_r2iq.h
+++ Core/fft_mt_r2iq/fft_mt_r2iq.h
@@ -22,6 +22,44 @@ static const int BASE_FFT_SCRAP_SIZE = 2048;
 static const int BASE_FFT_SIZE = FFTN_R_ADC + BASE_FFT_SCRAP_SIZE;
 static const int BASE_FFT_HALF_SIZE = BASE_FFT_SIZE / 2;
 
+// These helpers are compiled once per SIMD implementation translation unit.
+// Internal linkage is required: compiling one shared inline member under
+// /arch:AVX, /arch:AVX2, and /arch:AVX512 gives every copy the same COMDAT
+// name, allowing MSVC's linker to select the AVX-512 copy for lower kernels.
+template<bool rand>
+static inline void r2iq_convert_float(const int16_t* input, float* output, int size)
+{
+    for (int m = 0; m < size; m++)
+    {
+        int16_t val;
+        if (rand && (input[m] & 1))
+            val = input[m] ^ (-2);
+        else
+            val = input[m];
+        output[m] = float(val);
+    }
+}
+
+static inline void r2iq_shift_freq(fftwf_complex* dest, const fftwf_complex* source1,
+    const fftwf_complex* source2, int start, int end)
+{
+    for (int m = start; m < end; m++)
+    {
+        dest[m][0] = source1[m][0] * source2[m][0] - source1[m][1] * source2[m][1];
+        dest[m][1] = source1[m][1] * source2[m][0] + source1[m][0] * source2[m][1];
+    }
+}
+
+template<bool flip>
+static inline void r2iq_copy(fftwf_complex* dest, const fftwf_complex* source, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        dest[i][0] = source[i][0];
+        dest[i][1] = flip ? -source[i][1] : source[i][1];
+    }
+}
+
 struct r2iqThreadArg;
 
 class fft_mt_r2iq
@@ -57,129 +95,52 @@ public:
 
     float setFreqOffset(float offset);
 
-protected:
-
-    template<bool rand> void convert_float(const int16_t *input, float* output, int size)
-    {
-        for(int m = 0; m < size; m++)
-        {
-            int16_t val;
-            if (rand && (input[m] & 1))
-            {
-                val = input[m] ^ (-2);
-            }
-            else
-            {
-                val = input[m];
-            }
-            output[m] = float(val);
-        }
-    }
-
-    void shift_freq(fftwf_complex* dest, const fftwf_complex* source1, const fftwf_complex* source2, int start, int end)
-    {
-        for (int m = start; m < end; m++)
-        {
-            // besides circular shift, do complex multiplication with the lowpass filter's spectrum
-            // (a+ib)(c+id) = (ac - bd) + i(ad + bc)
-            dest[m][0] = source1[m][0] * source2[m][0] - source1[m][1] * source2[m][1];
-            dest[m][1] = source1[m][1] * source2[m][0] + source1[m][0] * source2[m][1];
-        }
-    }
-
-    template<bool flip> void copy(fftwf_complex* dest, const fftwf_complex* source, int count)
-    {
-        if (flip)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                dest[i][0] = source[i][0];
-                dest[i][1] = -source[i][1];
-            }
-        }
-        else
-        {
-            for (int i = 0; i < count; i++)
-            {
-                dest[i][0] = source[i][0];
-                dest[i][1] = source[i][1];
-            }
-        }
-    }
-
 private:
-    bool r2iqOn;        // r2iq on flag
-
-    ringbuffer<int16_t>* inputbuffer;    // pointer to input buffers
+    bool r2iqOn;
+    ringbuffer<int16_t>* inputbuffer;
     size_t inputbuffer_block_size = 0;
-
-    ringbuffer<float>* outputbuffer;    // pointer to output buffers
-
-    // --- Decimation --- //
-    int decimation = 0;   // selected decimation ratio
-      // 64 Msps:               0 => 32Msps, 1=> 16Msps, 2 = 8Msps, 3 = 4Msps, 4 = 2Msps
-      // 128 Msps: 0 => 64Msps, 1 => 32Msps, 2=> 16Msps, 3 = 8Msps, 4 = 4Msps, 5 = 2Msps
-    int decimation_ratio[NDECIDX];  // ratio
-
-    // Lookup table linking the decimation level to the size of the resulting FFT
-    // Each step divides the fft size by 2
-    // Definition : fft_size_per_decimation[x] = FFT_HALF_SIZE / 2^k
+    ringbuffer<float>* outputbuffer;
+    int decimation = 0;
+    int decimation_ratio[NDECIDX];
     int fft_size_per_decimation[NDECIDX];
-    // --- //
-
-    bool stateADCRand;       // randomized ADC output
+    bool stateADCRand;
     bool useSidebandLSB;
-
-    // number of ffts needed to process one block of the input buffer
     int ffts_per_blocks = 0;
-
-    int bufIdx;         // index to next buffer to be processed
+    int bufIdx;
     r2iqThreadArg* lastThread;
-
     float GainScale;
-
-    // The bin (the portion of the FFT result) in which
-    // the desired center frequency is located
     int center_frequency_bin = 0;
-
-    void *r2iqThreadf(r2iqThreadArg *th);   // thread function
-
+    void *r2iqThreadf(r2iqThreadArg *th);
     void * r2iqThreadf_generic(r2iqThreadArg *th);
     void * r2iqThreadf_avx(r2iqThreadArg *th);
     void * r2iqThreadf_avx2(r2iqThreadArg *th);
     void * r2iqThreadf_avx512(r2iqThreadArg *th);
     void * r2iqThreadf_neon(r2iqThreadArg *th);
-
-    fftwf_complex **filterHw;       // Hw complex to each decimation ratio
-
-	fftwf_plan  plan_time2freq_r2c;      // fftw plan buffers Freq to Time complex to complex per decimation ratio
-	fftwf_plan *plan_freq2time;          // fftw plan buffers Time to Freq real to complex per buffer
-	fftwf_plan  plan_freq2time_per_decimation[NDECIDX];
-
+    fftwf_complex **filterHw;
+    fftwf_plan plan_time2freq_r2c;
+    fftwf_plan *plan_freq2time;
+    fftwf_plan plan_freq2time_per_decimation[NDECIDX];
     uint32_t processor_count;
     r2iqThreadArg* threadArgs[N_MAX_R2IQ_THREADS];
-    std::thread r2iq_thread[N_MAX_R2IQ_THREADS]; // thread pointers
+    std::thread r2iq_thread[N_MAX_R2IQ_THREADS];
 };
 
-// assure, that ADC is not oversteered?
 struct r2iqThreadArg {
-
-	r2iqThreadArg()
-	{
+    r2iqThreadArg()
+    {
 #if PRINT_INPUT_RANGE
-		MinMaxBlockCount = 0;
-		MinValue = 0;
-		MaxValue = 0;
+        MinMaxBlockCount = 0;
+        MinValue = 0;
+        MaxValue = 0;
 #endif
-	}
-
+    }
     uint8_t thread_id;
-	float *ADCinTime;         // point to each threads input buffers [nftt][n]
-	fftwf_complex *ADCinFreq; // buffers in frequency
-	fftwf_complex *inFreqTmp; // tmp decimation output buffers (after tune shift)
+    float *ADCinTime;
+    fftwf_complex *ADCinFreq;
+    fftwf_complex *inFreqTmp;
 #if PRINT_INPUT_RANGE
-	int MinMaxBlockCount;
-	int16_t MinValue;
-	int16_t MaxValue;
+    int MinMaxBlockCount;
+    int16_t MinValue;
+    int16_t MaxValue;
 #endif
 };
